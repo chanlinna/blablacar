7d6a647938a198805f99e7e0d14f4dc1556ff962
chanlinna <chanlinnalin@gmail.com>
2026-02-06 09:57:07 +0700

    BLA-001- Rides Service

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,12 +1,34 @@
 import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> _filterByDeparture(Location departure) {
+    return availableRides
+        .where((r) => r.departureLocation == departure)
+        .toList();
+  }
 
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> _filterBySeatRequested(int requestedSeat) {
+    return availableRides
+        .where((r) => r.availableSeats == requestedSeat)
+        .toList();
+  }
+
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return availableRides.where((a) => _filterByDeparture(departure!))
+
+    
+  }
 }
--- test/test_rides.dart
+++ test/test_rides.dart
@@ -1,12 +1,12 @@
-import 'package:blabla/model/ride/ride.dart';
+import 'package:blablacar/model/ride/ride.dart';
 
-import 'package:blabla/model/ride/locations.dart';
-import 'package:blabla/services/rides_service.dart';
+import 'package:blablacar/model/ride/locations.dart';
+import 'package:blablacar/services/rides_service.dart';
 
 void main() {
   Location dijon = Location(country: Country.france, name: "Dijon");
 
-  List<Ride> filteredRide = RidesService.filter(requestedSeats: 2, departureLocation: dijon);
+  List<Ride> filteredRide = RidesService.filterBy(seatRequested:2, departure:dijon);
 
   for (Ride ride in filteredRide) {
     print(ride);
